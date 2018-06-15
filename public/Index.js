7390cdca27989a31bf6b9e0672fa5dafb3369d97
krishnkant <krishnkantnayak02@gmail.com>
2018-06-16 00:52:38 +0530

    initial commit

---
+++ public/Index.js
@@ -0,0 +1,13 @@
+import React  from 'react'
+import  {render} from 'react-dom'
+import Login from './js/components/Login'
+
+
+    render(
+        <Login />,
+         document.getElementById("root")
+        )
+
+
+
+   
---
+++ run.sh
@@ -0,0 +1,3 @@
+npm run webpack 
+npm start
+
